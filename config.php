5316b7301bb971b90f3be22e764342d69ea1657e
shimaa ghoneim <76048440+shimaa279@users.noreply.github.com>
2021-05-21 21:10:40 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+
+ <?php
+$con = mysqli_connect("localhost","root","","social_100010339");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+
+}
+?>
